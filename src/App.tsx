5023f849fffd57c7370852dbb0847601b4ebd8f6
Toni Mertanen <t.mertanen@hotmail.com>
2024-06-09 16:57:40 +0300

    Exercise 3 for manipulating objects in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy pepperoni",
-    toppings: ["mushrooms"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      <p>Exercise 2 for mutating objects</p>
+      <p>Exercise 3 for mutating objects</p>
     </div>
   );
 }
